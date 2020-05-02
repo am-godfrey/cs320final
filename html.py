af8022b2a75814631b13b076ce7f7a38b164fd36
thetiltedone <42614362+thetiltedone@users.noreply.github.com>
2020-05-03 03:30:45 +0800

    Add files via upload
    html.py opens the websites

---
+++ html.py
@@ -0,0 +1,27 @@
+@app.route('/index.html')
+def index():
+    with open("index.html") as f:
+        html = f.read()
+    return html
+
+@app.route('/data.html')
+def data():
+    with open("data.html") as f:
+        html = f.read()
+    return html
+
+@app.route('/acks.html')
+def acks():
+    with open("acks.html") as f:
+        html = f.read()
+    return html
+
+@app.route('/results.html')
+def acks():
+    with open("results.html") as f:
+        html = f.read()
+    return html
+    
+    
+if __name__ == '__main__':
+    app.run(host="0.0.0.0") # don't change this line!
